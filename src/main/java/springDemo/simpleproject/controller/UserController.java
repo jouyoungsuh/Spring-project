6d604516d1ab73d31f893584be67250348879fb6
jouyoungsuh <jouyoungsuh@gmail.com>
2021-05-26 00:27:01 -0400

    Created routes for the sign up and showing user list.

--- src/main/java/springDemo/simpleproject/controller/UserController.java
+++ src/main/java/springDemo/simpleproject/controller/UserController.java
@@ -2,7 +2,13 @@ package springDemo.simpleproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import springDemo.simpleproject.domain.User;
 import springDemo.simpleproject.service.UserService;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -11,6 +17,26 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/users/new")
+    public String createForm(){
+        return "users/createNewUserForm-template";
+    }
+
+    @PostMapping(value = "/users/new")
+    public String createUser(UserForm form) {
+        User member = new User();
+        member.setName(form.getName());
+        userService.signUp(member);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/users")
+    public String list(Model model) {
+        List<User> users = userService.findUsers();
+        model.addAttribute("users", users);
+        return "users/userList-template";
     }
 
 }
