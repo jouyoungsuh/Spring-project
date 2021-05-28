ad3b948aaa2f5f5dc4a215e6d29a7e2e6686c7d4
jouyoungsuh <jouyoungsuh@gmail.com>
2021-05-28 01:44:28 -0400

    Implemented JDBC; changed the temporary (hashmap) database into H2 (relational) database system. Added dependencies, made changes in the configuation using spring beans. Major changes on repostitory (named as UserRepositoryJDBC)

--- src/main/java/springDemo/simpleproject/SpringConfig.java
+++ src/main/java/springDemo/simpleproject/SpringConfig.java
@@ -3,14 +3,24 @@ package springDemo.simpleproject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springDemo.simpleproject.repository.UserRepository;
+import springDemo.simpleproject.repository.UserRepositoryInterface;
+import springDemo.simpleproject.repository.UserRepositoryJDBC;
 import springDemo.simpleproject.service.UserService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
-    public UserRepository userRepository(){
-        return new UserRepository();
+    public UserRepositoryInterface userRepository(){
+        //return new UserRepository();
+        return new UserRepositoryJDBC(dataSource);
     }
 
     @Bean
---
+++ src/main/java/springDemo/simpleproject/repository/UserRepositoryJDBC.java
@@ -0,0 +1,175 @@
+package springDemo.simpleproject.repository;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import springDemo.simpleproject.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+public class UserRepositoryJDBC implements UserRepositoryInterface {
+    private final DataSource dataSource;
+    public UserRepositoryJDBC(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public User save(User user) {
+        String sql = "insert into user(name) values(?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, user.getName());
+            pstmt.executeUpdate();
+            rs = pstmt.getGeneratedKeys();
+
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+            }
+            else {
+                throw new SQLException("Cannot find ID");
+            }
+            return user;
+        }
+        catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public Optional<User> findById(Integer id) {
+        String sql = "select * from user where id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            rs = pstmt.executeQuery();
+
+            if(rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                return Optional.of(user);
+            }
+            else {
+                return Optional.empty();
+            }
+        }
+        catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        String sql = "select * from user";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            List<User> users = new ArrayList<>();
+            while(rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                users.add(user);
+            }
+            return users;
+        }
+
+        catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public Optional<User> findByName(String name) {
+        String sql = "select * from user where name = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            rs = pstmt.executeQuery();
+
+            if(rs.next())
+            {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                return Optional.of(user);
+            }
+            return Optional.empty();
+        }
+
+        catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
+    {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+}
+
